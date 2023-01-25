13c95adae7cabcb272c12135c4989426991f8748
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-01-25 15:48:45 +0200

    Create NavBar Component

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,50 @@
+import { motion } from 'framer-motion'
+import { useState } from 'react'
+import { HiMenuAlt4, HiX } from 'react-icons/hi'
+import { v4 as uuidv4 } from 'uuid'
+
+import images from '../../constants/images'
+
+import './Navbar.scss'
+
 const Navbar = () => {
-	return <div>Navbar</div>
+	const [toggle, setToggle] = useState(false)
+	const navMenu = ['home', 'about', 'contact', 'work', 'skills']
+	return (
+		<nav className='app__navbar'>
+			<div className='app__navbar-logo'>
+				<img src={images.logo} alt='logo' />
+			</div>
+			<ul className='app__navbar-links'>
+				{navMenu.map(item => (
+					<li key={uuidv4()} className='app__flex p-text'>
+						<div />
+						<a href={`#${item}`}>{item}</a>
+					</li>
+				))}
+			</ul>
+			<div className='app__navbar-menu'>
+				<HiMenuAlt4 onClick={() => setToggle(true)} />
+				{toggle && (
+					<motion.div
+						whileInView={{ x: [300, 0] }}
+						transition={{ duration: 0.85, ease: 'easeOut' }}
+					>
+						<HiX onClick={() => setToggle(false)} />
+						<ul>
+							{navMenu.map(item => (
+								<li key={uuidv4()}>
+									<a onClick={() => setToggle(false)} href={`#${item}`}>
+										{item}
+									</a>
+								</li>
+							))}
+						</ul>
+					</motion.div>
+				)}
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar
