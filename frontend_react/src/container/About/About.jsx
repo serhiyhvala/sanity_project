39a8b0a378dac75894163cd80803212aa47e5898
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-01-26 21:47:20 +0200

    Refactor About Section with Sanity

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,28 +2,23 @@ import { motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
-import images from '../../constants/images'
+import { client, urlFor } from '../../client'
 
 import './About.scss'
 
 const About = () => {
-	const abouts = [
-		{
-			title: 'Web Development',
-			description: 'I am good a Web Developer',
-			imgUrl: images.about01
-		},
-		{
-			title: 'Web Design',
-			description: 'I am good a Web Design',
-			imgUrl: images.about02
-		},
-		{
-			title: 'Web Animation',
-			description: 'I am good a Web Animation',
-			imgUrl: images.about03
+	const [abouts, setAbouts] = useState([])
+	useEffect(() => {
+		const query = '*[_type=="abouts"]'
+		const fetchData = async () => {
+			try {
+				return await client.fetch(query)
+			} catch (e) {
+				console.error(e)
+			}
 		}
-	]
+		fetchData().then(data => setAbouts(data))
+	}, [])
 	return (
 		<>
 			<h2 className='head-text'>
@@ -39,7 +34,7 @@ const About = () => {
 						transition={{ duration: 0.5, type: 'tween' }}
 						className='app__profile-item'
 					>
-						<img src={item.imgUrl} alt={item.title} />
+						<img src={urlFor(item.imgUrl)} alt={item.title} />
 						<h2 className='bold-text' style={{ marginTop: 20 }}>
 							{item.title}
 						</h2>
